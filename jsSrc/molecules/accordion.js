1267859c17735d0026c5a295b0a226886ac58fbf
Raphael Hüni <rafhungmail.com>
2015-07-29 06:04:58 +0200

    fix(js): accordionToggle lookup conditional
    We need to look up the accordion toggle information conditionally since
    an error is thrown if the element cannot be found. Therefore we first
    check if an accordion toggle is on the page and only then extract the
    information. For future stuff maybe think about adding all config stuff
    to a global array then add this array to the body:before content.

--- jsSrc/molecules/accordion.js
+++ jsSrc/molecules/accordion.js
@@ -2,11 +2,14 @@
 // the class .in on an element on click
 
 jQuery(document).ready(function($) {
-  var accordionToggle = $('[data-js="accordion-toggle"]'),
+  var accordionToggle = $('[data-js="accordion-toggle"]');
     // here we get the computed style of the before element of the first accordionToggle that is found on the page which is defined to just print out the currently configured $is-open class. This makes our JS easily adaptable to a user's preferences.
-    isOpenContent = window.getComputedStyle(accordionToggle[0], ':before').content,
+    // However we can only look for the value if there actually is at least one accordion toggle on the page, therefore we check for its existence.
+  if (accordionToggle.length) {
+    var isOpenContent = window.getComputedStyle(accordionToggle[0], ':before').content,
     // as the above returns the concrete value (i. e.: '.is-open') we need to remove the quotes as well as the dot to get a good value for toggleClass. We can do this by using the substring method, starting at 2 and running for length-1. This removes the first two characters as well as the very last one and will work if the mixin is configured correctly
     isOpen = isOpenContent.substring(2, isOpenContent.length-1);
+  }
 
   // if the accordionToggle is clicked we toggle the isOpen class
   accordionToggle.click(function() {
