9dd4ea66bf40a09287bc4258a60bc81568cc13a4
Raphael Hüni <hueni@werbelinie.ch>
2015-07-31 09:39:03 +0200

    feat(grunt): add changelog reset preset
    We add a commented out preset that will allow us to regenerate the whole
    changelog by activating the option line.

--- grunt/conventionalChangelog.js
+++ grunt/conventionalChangelog.js
@@ -2,7 +2,9 @@ module.exports = {
   options: {
     changelogOpts: {
       // conventional-changelog options go here
-      preset: 'angular'
+      preset: 'angular',
+      // activate the following line to regenarate your changelog (in case some commits went missing from it), make sure to delete the contents of CHANGELOG.md before)
+      // releaseCount: 0
     },
     context: {
       // context goes here
