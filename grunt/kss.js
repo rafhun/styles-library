5e5e3a8ca114da4f2f40a58e094ade6dc5ddcc9d
Raphael Hüni <hueni@werbelinie.ch>
2015-11-10 09:52:01 +0100

    feat(kss): add custom kss template
    Prepare the use of a custom kss template. For now we just remove the max width on the kss-sections
    but the further goal is to create a complete own styleguide template that fits our brand.
    
    Closes #9

--- grunt/kss.js
+++ grunt/kss.js
@@ -3,7 +3,7 @@ module.exports = {
     css: ['css/style.css', 'css/icons.data.svg.css'],
     js: 'js/script.js',
     // npm: 2 // uncomment if you are using npm v2.*
-    // template: 'node_modules/kss/generator/handlebars/template/'
+    template: 'kss/template/'
   },
   styleguide: {
     files: {
